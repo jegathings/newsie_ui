a99a938189dcee058ed8a4504b8776516fd4a6b8
James <jegathings@gmail.com>
2020-10-03 16:11:54 -0400

    Successfully connects to the NYT API and renders the title of each article.

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ import './css/style.scss';
 
 
 const App = (props) => {
+    const [articles, setArticles] = React.useState(null);
     const baseURL = 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=hxJymqaBJ3aNPRUChT9AlODBoYFOgAMx';    
     
     const getInfo = async() =>{
         const response = await fetch(`${baseURL}`);
         const result = await response.json();
-        setBookmarks(result.results);
+        console.log(result.results);
+        setArticles(result.results);
     }
 
     React.useEffect(() => {
@@ -21,6 +23,26 @@ const App = (props) => {
         <>
             <div className="main">
                 <h1>Newsie</h1>
+                <div>
+                    {
+                    articles ? 
+                    articles.map((article, index) => {
+                        return(
+                            <div key={index}>
+                                <div className="main-list-item">
+                                <div className="main-list-item-div main-list-item-div-one">
+                                <a href={article.short_url} target="_blank"><button>{article.title}</button></a>
+                                </div>
+                                <div className="main-list-item-div main-list-item-div-two">
+                                </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                    :
+                    "...Loading"                        
+                    }
+                </div>
             </div>
         </>
     );
