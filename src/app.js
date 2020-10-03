e446800c98e06e1e4d4ecab527d28f6c005e3963
James <jegathings@gmail.com>
2020-10-03 18:06:41 -0400

    Click on an item and it is stored in a set.  The set will then be persisted to the db.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import './css/style.scss';
 
 const App = (props) => {
     const [articles, setArticles] = React.useState(null);
+    const [count, setCount] = React.useState(null);
+    const [savedArticles, setSavedArticles] = React.useState(new Set(), null);
     const baseURL = 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=hxJymqaBJ3aNPRUChT9AlODBoYFOgAMx';    
     
     const getInfo = async() =>{
@@ -18,11 +20,27 @@ const App = (props) => {
     React.useEffect(() => {
         getInfo()
     },[]);
-
+    const handleSave = (article) =>{
+        savedArticles.add(article.url);
+        setSavedArticles(savedArticles)
+        setCount(savedArticles.size);
+    }
+    const handlePersist = () =>{
+        console.log("To Database");
+        console.log(savedArticles);
+    }
     return (
         <>
             <div className="main">
-                <h1>Newsie</h1>
+                <div className="title">
+                    <h1>Newsie</h1>
+                    <div className="cart">
+                        <h1>{count}</h1>
+                        <button onClick={() =>{
+                                        handlePersist();
+                                    }}>save</button>
+                    </div>
+                </div>
                 <div>
                     {
                     articles ? 
@@ -31,9 +49,9 @@ const App = (props) => {
                             <div key={index}>
                                 <div className="main-list-item">
                                 <div className="main-list-item-div main-list-item-div-one">
-                                <a href={article.short_url} target="_blank"><button>{article.title}</button></a>
-                                </div>
-                                <div className="main-list-item-div main-list-item-div-two">
+                                <button onClick={() =>{
+                                        handleSave(article);
+                                    }}>{article.title}</button>
                                 </div>
                                 </div>
                             </div>
